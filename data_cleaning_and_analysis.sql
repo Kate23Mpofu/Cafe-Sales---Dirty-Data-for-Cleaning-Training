ccb2637ccccbeb063ddd3c3f87fa6406b25526f5
maMpofu <136378456+Kate23Mpofu@users.noreply.github.com>
2026-05-31 19:44:48 +0200

    Handle missing values in cafe_sales_clean
    Added SQL queries to identify and handle missing values for total_spent, payment_method, location, and transaction_date. Included checks for positive quantity and price_per_unit.

--- data_cleaning_and_analysis.sql
+++ data_cleaning_and_analysis.sql
@@ -86,7 +86,98 @@ WHERE
     OR payment_method = '')
     AND (location IS NULL OR location = '');
     
+-- identify missing total_spent amounts
+SELECT 
+    quantity, price_per_unit, total_spent
+FROM
+    cafe_sales_clean
+WHERE
+    total_spent IS NULL OR total_spent = ''
+        OR total_spent = 'ERROR'
+        OR total_spent = 'UNKNOWN';
+
+-- handle missing total_spent amounts
+-- the total_spent is quantity * price_per_unit
+UPDATE cafe_sales_clean 
+SET 
+    total_spent = (quantity * price_per_unit)
+WHERE
+    total_spent IS NULL OR total_spent = ''
+        OR total_spent = 'ERROR'
+        OR total_spent = 'UNKNOWN';
+        
+-- find missing payment methods
 SELECT 
     *
 FROM
-    cafe_sales_clean;
+    cafe_sales_clean
+WHERE
+    payment_method IS NULL
+        OR payment_method = ''
+        OR payment_method = 'ERROR'
+        OR payment_method = 'UNKNOWN'
+;
+
+-- handle missing payment methods
+UPDATE cafe_sales_clean 
+SET 
+    payment_method = 'UNKNOWN'
+WHERE
+    payment_method IS NULL
+        OR payment_method = ''
+        OR payment_method = 'ERROR'
+        OR payment_method = 'UNKNOWN'
+;
+
+-- find missing locations
+SELECT 
+    *
+FROM
+    cafe_sales_clean
+WHERE
+    location IS NULL OR location = ''
+        OR location = 'ERROR'
+        OR location = 'UNKNOWN'
+;
+
+-- handle missing locations
+UPDATE cafe_sales_clean 
+SET 
+    location = 'UNKNOWN'
+WHERE
+    location IS NULL OR location = ''
+        OR location = 'ERROR'
+        OR location = 'UNKNOWN'
+;
+
+-- find missing dates
+SELECT 
+    *
+FROM
+    cafe_sales_clean
+WHERE
+    transaction_date IS NULL
+        OR transaction_date = ''
+        OR transaction_date = 'ERROR'
+        OR transaction_date = 'UNKNOWN'
+;
+
+-- handle missing dates
+UPDATE cafe_sales_clean 
+SET 
+    transaction_date = 'UNKNOWN'
+WHERE
+    transaction_date IS NULL
+        OR transaction_date = ''
+        OR transaction_date = 'ERROR'
+        OR transaction_date = 'UNKNOWN'
+;
+
+-- check if quantity and prices per unit are positive
+SELECT 
+    quantity, price_per_unit
+FROM
+    cafe_sales_clean
+WHERE
+    quantity < 1;
+
